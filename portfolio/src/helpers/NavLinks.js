84137cfb0c93f869e2cabe39e11c0669ba140943
Josh Elrod <elrodjosh96@gmail.com>
2022-03-29 17:34:30 -0400

    npms added, navbar added

---
+++ portfolio/src/helpers/NavLinks.js
@@ -0,0 +1,22 @@
+export const NavLinks = [
+    {
+        name: 'HOME',
+        ref: '#home'
+    },
+    {
+        name: 'ABOUT ME',
+        ref: '#about-me'
+    },
+    {
+        name: 'TECHNOLOGIES',
+        ref: '#technologies'
+    },
+    {
+        name: 'PORTFOLIO',
+        ref: '#portfolio'
+    },
+    {
+        name: 'CONTACT',
+        ref: '#contact'
+    },
+]
